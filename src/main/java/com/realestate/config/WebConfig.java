3cf8e8ef27ef85bc099063d3c83f2a52fb5bbe0b
Shubham Vasant <laxmankhatke1@gmail.com>
2026-04-22 10:55:47 +0530

    cors error resolved

---
+++ src/main/java/com/realestate/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.realestate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://www.1guntha.com",
+                                "https://www.1guntha.com"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
